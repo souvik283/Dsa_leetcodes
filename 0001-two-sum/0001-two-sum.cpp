de635f12864672ca333372e2713d41baba0f6059
Souvik Ghosh <ghosh2005202318@gmail.com>
2026-08-12 01:48:50 +0530

    Time: 41 ms (26.33%), Space: 14 MB (85.5%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       int n = nums.size();
+       vector<int> output (2,0);
+
+       for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if((nums[i]+ nums[j]) == target){
+                output[0] = i;
+                output[1] = j;
+                break;
+            }
+        }
+       }
+       return output;
+    }
+};
