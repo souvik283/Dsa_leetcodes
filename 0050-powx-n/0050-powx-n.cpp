b55f70fd5794157f44d131cc919a310f7737a916
Souvik Ghosh <ghosh2005202318@gmail.com>
2026-08-13 00:41:27 +0530

    Time: 0 ms (100%), Space: 8.4 MB (93.57%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     double myPow(double x, long int n) {
+        if(x == 0.0) return 0.0;
+        if(x == 1.0) return 1.0;
+        if(n == 0) return 1.0;
+        if(x == -1 && n%2 == 0) return 1.0;
+        if(x == -1 && n%2 != 0) return -1.0;
+
         double ans = 1;
         if(n<0){
             n = -n;
