9374a8a193fa17bac20139350dfcbfdd130f52b6
Souvik Ghosh <ghosh2005202318@gmail.com>
2026-08-13 00:34:19 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.65%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, long int n) {
+        double ans = 1;
+        if(n<0){
+            n = -n;
+            x = 1/x;
+        }
+       
+       while(n>0){
+        if(n%2 == 1){
+            ans *= x;
+        }
+        x *= x;
+        n /= 2;
+       }
+
+        return ans;
+        
+    }
+};
