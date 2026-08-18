d62c82ecc050405ec9043a2a9843ed68a3ed8b79
Souvik Ghosh <ghosh2005202318@gmail.com>
2026-08-19 00:32:28 +0530

    Time: 3 ms (28.58%), Space: 10 MB (35.96%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    bool iaAlphaNum (char ch){
+        if((ch >= '0' && ch <= '9') || ((tolower(ch) >= 'a' && tolower(ch) <= 'z'))){
+            return true;
+        }
+        return false;
+    }
+
+    bool isPalindrome(string s) {
+        int st=0, end=s.length()-1;
+
+        while(st<=end){
+
+            if(!iaAlphaNum(s[st])){
+                st++;
+                continue;
+            }
+
+            if(!iaAlphaNum(s[end])){
+                end--;
+                continue;
+            }
+
+            if(tolower(s[st]) != tolower(s[end])){
+                return false;
+            }
+
+            st++;
+            end--;
+        }
+
+        return true;
+        
+    }
+};
