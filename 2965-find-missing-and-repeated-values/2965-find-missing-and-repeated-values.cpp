f39f7485f6297335d9d2cb1f68099895eae2693f
Souvik Ghosh <ghosh2005202318@gmail.com>
2026-08-24 00:50:18 +0530

    Time: 13 ms (43.05%), Space: 31.7 MB (37.13%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -4,22 +4,33 @@ public:
         int m = size(grid);
         vector<int> ans;
         unordered_set<int> s;
+        int a, b;
+        int actualSum =0, expSum=0;
 
         for(int i=0 ; i< m ; i++ ){
             for(int j=0 ; j< m ; j++ ){
+
+                actualSum += grid[i][j];
+
                 if(s.count(grid[i][j])){
                     ans.push_back(grid[i][j]);
+                    a= grid[i][j];
                 }
                 s.insert(grid[i][j]);
             }
         }
 
-        for(int i=1; i<= m*m; i++){
-            if(s.find(i) == s.end()){
-                ans.push_back(i);
-                break;
-            }
-        }
+        expSum =(m*m) * (m*m +1) /2;
+
+        b= expSum - actualSum +a;
+        ans.push_back(b);
+
+        // for(int i=1; i<= m*m; i++){
+        //     if(s.find(i) == s.end()){
+        //         ans.push_back(i);
+        //         break;
+        //     }
+        // }
 
         return ans;
     }
