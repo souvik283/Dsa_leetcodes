470d66def03db2989c43730ba176f0b7ec376263
Souvik Ghosh <ghosh2005202318@gmail.com>
2026-08-26 00:54:10 +0530

    Time: 47 ms (63.43%), Space: 29.2 MB (28.54%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -8,9 +8,7 @@ public:
 
         for(int i =0; i<n; i++){
 
-            if(i>0 && nums[i] == nums[i-1]){
-                continue;
-            }
+            if(i>0 && nums[i] == nums[i-1]) continue;
 
             j=i+1; k=n-1;
 
@@ -20,9 +18,7 @@ public:
                     ans.push_back({nums[i], nums[j], nums[k]});
                     j++; k--;
 
-                    while(j<k && nums[j]==nums[j-1]) {
-                        j++;
-                    }
+                    while(j<k && nums[j]==nums[j-1]) j++;
 
                 }else if(sum > 0){
                     k--;
