d90f43e96ea9b0a17aba157763010c85996031a0
Souvik Ghosh <ghosh2005202318@gmail.com>
2026-09-05 02:11:02 +0530

    Time: 12 ms (32.01%), Space: 7.9 MB (38.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
 
-    int fibonacci(int n){
+    int fib(int n) {
         if(n==0 || n==1){
             return n;
         }
-        return fibonacci(n-1) + fibonacci(n-2);
-    }
-    int fib(int n) {
-        return fibonacci(n);
+        return fib(n-1) + fib(n-2);
     }
 };
